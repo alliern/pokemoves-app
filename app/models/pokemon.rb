fc50e78d2fb25c03d1fb5c8dff130ac6d3eda7fc
Allie Neal <allieneal93@gmail.com>
2021-04-16 17:06:41 -0400

    tries to add model methods

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,11 @@
 class Pokemon < ApplicationRecord
   has_many :poke_moves
   has_many :moves, through: :poke_moves
+
+
+  def empty_type
+    if pokemon.type_2 != true
+      pokemon.type_2 = "N/A"
+    end
+  end
 end
