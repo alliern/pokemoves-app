6467a05d54c6289e9865de7bcf047eb5276537e5
Allie Neal <allieneal93@gmail.com>
2021-04-29 16:59:42 -0400

    updates views

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon < ApplicationRecord
   has_many :poke_moves
   has_many :moves, through: :poke_moves
 
-  def move
-    move = pokemon.poke_moves.moves
-    move
-  end
+  # def move
+  #   move = pokemon.poke_moves.moves
+  #   move
+  # end
 
 end
