0c57386c21d847d69133f0e20922ddc0eb0f76db
Allie Neal <allieneal93@gmail.com>
2021-05-05 16:57:50 -0400

    fixes some controller syntax

--- app/controllers/api/user_pokemon_controller.rb
+++ app/controllers/api/user_pokemon_controller.rb
@@ -12,24 +12,24 @@ class Api::UserPokemonController < ApplicationController
   end
 
   def create
-    @user_pokemon = UserPokemon.new(
+    user_pokemon = UserPokemon.new(
       user_id: current_user.id,
       pokemon_id: params[:pokemon_id],
       pokemon_name: params[:pokemon_name]
     )
-    if @user_pokemon.save
+    if user_pokemon.save
       render json: {message: "User Pokemon created Successfully"}, status: :created
     else
-      render json: {errors: users.errors.full_messages}, status: :bad_request
+      render json: {errors: user_pokemon.errors.full_messages}, status: :bad_request
     end
   end
 
-  def update
-    @user_pokemon = UserPokemon.where(user_id: current_user.id)
-    @user_pokemon.user_id = current_user.id
-    @user_pokemon.pokemon_id = params[:pokemon_id] || @user_pokemon.pokemon_id
-    @user_pokemon.pokemon_name = params[:pokemon_name] || @user_pokemon.pokemon_name
-  end
+  # def update
+  #   @user_pokemon = UserPokemon.where(user_id: current_user.id)
+  #   @user_pokemon.user_id = current_user.id
+  #   @user_pokemon.pokemon_id = params[:pokemon_id] || @user_pokemon.pokemon_id
+  #   @user_pokemon.pokemon_name = params[:pokemon_name] || @user_pokemon.pokemon_name
+  # end
 
   def destroy
     @user_pokemon = UserPokemon.find_by(id: params[:id])
